328db947b518132545336088b5f8d90399e3a078
Llxovell <70419875+Llxovell@users.noreply.github.com>
2022-10-20 19:42:41 -0700

    added tailwind, updated timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,12 +2,12 @@ import { React, useState, useEffect } from "react";
 
 function Timer() {
   //useState to keep track of second value
-  const [seconds, setSeconds] = useState(5100);
+  const [seconds, setSeconds] = useState(3600);
 
   //Add timer that counts down by the second
   useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds((time) => time - 1);
+      setSeconds((time) => (time > 0 ? time - 1 : time));
     }, 1000);
     console.log((3600 % 3600) + 1);
     return () => {
@@ -27,24 +27,35 @@ function Timer() {
     } else formattedHours = hrs + ":";
 
     //format minutes
-    if (mins === 0) {
-      formattedMinutes = "00:";
-    } else if (mins < 10) {
+    if (mins === 0 && hrs <= 0) {
+      formattedMinutes = "";
+    } else if (mins < 10 && hrs !== 0) {
       formattedMinutes = "0" + mins.toString() + ":";
+    } else if (mins < 10) {
+      formattedMinutes = mins + ":";
     } else formattedMinutes = mins + ":";
 
     //format seconds
     if (secs === 0) {
       formattedSeconds = "00";
-    } else if (secs < 10) {
+    } else if (secs < 10 && mins !== 0) {
       formattedSeconds = "0" + secs.toString();
+    } else if (secs < 10) {
+      formattedSeconds = secs;
     } else formattedSeconds = secs;
+
+    //format timer at 0 secs
+    if (hrs === 0 && mins === 0 && secs === 0) {
+      formattedHours = "";
+      formattedMinutes = "";
+      formattedSeconds = "0";
+    }
 
     return formattedHours + formattedMinutes + formattedSeconds;
   }
 
   return (
-    <div>
+    <div className="text-3xl font-semibold">
       {/*Display timer*/}
       {ConvertSeconds()}
     </div>
